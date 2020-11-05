03187d94d31f28567ea1c95852b832f743392fbf
mikel <mikel123@hawaii.rr.com>
2020-11-04 19:54:17 -1000

    contacts collection finished in 24 minutes

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,12 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/contact/Contacts';
+import { Stuffs } from '../../api/stuff/Stuff';
 
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
 function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
+  console.log(`  Adding: ${data.Name} (${data.owner})`);
   Stuffs.collection.insert(data);
+}
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.collection.insert(data);
 }
 
 /** Initialize the collection if empty. */
@@ -14,5 +19,12 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default Contacts.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
   }
 }
