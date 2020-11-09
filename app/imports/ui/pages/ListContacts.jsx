a20eeef9bb5984c93e10674b8e20b99bbe69b920
mikel <mikel123@hawaii.rr.com>
2020-11-08 20:09:18 -1000

    notes-1 finished in 20 minutes

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -5,6 +5,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Contact from '../components/Contact';
 import { Contacts } from '../../api/contact/Contacts';
+import { Notes } from '../../api/note/Notes';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListContacts extends React.Component {
@@ -20,7 +21,9 @@ class ListContacts extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>List Contacts</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <Contact key={index}
+                                                                  contact={contact}
+                                                                  notes={this.props.notes.filter(note => (note.contactId === contact._id))}/>)}
           </Card.Group>
         </Container>
     );
@@ -31,14 +34,17 @@ class ListContacts extends React.Component {
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
+  notes: PropTypes.array.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscription2 = Meteor.subscribe(Notes.userPublicationName);
   return {
     contacts: Contacts.collection.find({}).fetch(),
-    ready: subscription.ready(),
+    notes: Notes.collection.find({}).fetch(),
+    ready: subscription.ready() && subscription2.ready(),
   };
 })(ListContacts);
